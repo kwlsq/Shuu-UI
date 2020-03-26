44dd94351c7368c2689aa7a77d5e8a96d0bb4d97
Vincent <vincentiussss@gmail.com>
2020-03-26 11:26:45 +0700

    change navbar into class component

--- src/Comps/navbar.jsx
+++ src/Comps/navbar.jsx
@@ -5,184 +5,190 @@ import { logout, keepLogin } from '../Redux/Actions';
 
 import '../CSS/navbar.css';
 
-const NavbarComp = (props) => {
-    props.keepLogin()
+class NavbarComp extends React.Component {
+    componentDidMount() {
+        this.props.keepLogin()
 
-    if (props.role_id === 3) {
-        console.log('ini user')
-        return (
-            <div className="navbar-wrapper">
-                <div className="nav-items">
-                    <div className="logo">
-                        <Link to='/'>
-                            Shuu
-                </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/men'>
-                            Men
+    }
+    render() {
+        if (this.props.role_id === 3) {
+            console.log('ini user')
+            return (
+                <div className="navbar-wrapper">
+                    <div className="nav-items">
+                        <div className="logo">
+                            <Link to='/'>
+                                Shuu
                     </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/women'>
-                            Women
-                    </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/feeds'>
-                            Feed
-                    </Link>
-                    </div>
-                    <div className="searchbar">
-                        <input type="text" placeholder="search"></input>
-                        <button><i className="fas fa-search"></i></button>
-                    </div>
-                    <div className="button-login">
-                        <Link to='/profile'>
-                            {props.username}
+                        </div>
+                        <div className="button">
+                            <Link to='/men'>
+                                Men
                         </Link>
-                    </div>
-                    <div className="button-login">
-                        <a href='/cart'>
-                            <i className="fas fa-shopping-bag"></i>
-                        </a>
-                    </div>
-                    <div className="button-register">
-                        <Link to='/' onClick={props.logout}>
-                            Log Out
-                    </Link>
-                    </div>
+                        </div>
+                        <div className="button">
+                            <Link to='/women'>
+                                Women
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/feeds'>
+                                Feed
+                        </Link>
+                        </div>
+                        <div className="searchbar">
+                            <input type="text" placeholder="search"></input>
+                            <button><i className="fas fa-search"></i></button>
+                        </div>
+                        <div className="button-login">
+                            <Link to='/profile'>
+                                {this.props.username}
+                            </Link>
+                        </div>
+                        <div className="button-login">
+                            <a href='/cart'>
+                                <i className="fas fa-shopping-bag"></i>
+                            </a>
+                        </div>
+                        <div className="button-register">
+                            <Link to='/' onClick={this.props.logout}>
+                                Log Out
+                        </Link>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        )
-    } else if (props.role_id === 2) {
-        console.log('ini toko/partner')
-        return (
-            <div className="navbar-wrapper">
-                <div className="nav-items">
-                    <div className="logo">
-                        <Link to='/'>
-                            Shuu
-                </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/men'>
-                            Men
+            )
+        } else if (this.props.role_id === 2) {
+            console.log('ini toko/partner')
+            return (
+                <div className="navbar-wrapper">
+                    <div className="nav-items">
+                        <div className="logo">
+                            <Link to='/'>
+                                Shuu
                     </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/women'>
-                            Women
-                    </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/feeds'>
-                            Feed
-                    </Link>
-                    </div>
-                    <div className="searchbar">
-                        <input type="text" placeholder="search"></input>
-                        <button><i className="fas fa-search"></i></button>
-                    </div>
+                        </div>
+                        <div className="button">
+                            <Link to='/men'>
+                                Men
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/women'>
+                                Women
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/feeds'>
+                                Feed
+                        </Link>
+                        </div>
+                        <div className="searchbar">
+                            <input type="text" placeholder="search"></input>
+                            <button><i className="fas fa-search"></i></button>
+                        </div>
 
-                    <div className="button-login">
-                        <Link to='/store'>
-                            {props.username}
+                        <div className="button-login">
+                            <Link to='/store'>
+                                {this.props.username}
+                            </Link>
+                        </div>
+                        <div className="button-register">
+                            <Link to='/' onClick={this.props.logout}>
+                                Log Out
                         </Link>
-                    </div>
-                    <div className="button-register">
-                        <Link to='/' onClick={props.logout}>
-                            Log Out
-                    </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    } else if (props.role_id === 1) {
-        console.log('ini admin')
-        return (
-            <div className="navbar-wrapper">
-                <div className="nav-items">
-                    <div className="logo">
-                        <Link to='/'>
-                            Shuu
-                </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/men'>
-                            Men
+            )
+        } else if (this.props.role_id === 1) {
+            console.log('ini admin')
+            return (
+                <div className="navbar-wrapper">
+                    <div className="nav-items">
+                        <div className="logo">
+                            <Link to='/'>
+                                Shuu
                     </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/women'>
-                            Women
-                    </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/feeds'>
-                            Feed
-                    </Link>
-                    </div>
-                    <div className="searchbar">
-                        <input type="text" placeholder="search"></input>
-                        <button><i className="fas fa-search"></i></button>
-                    </div>
-                    <div className="button-login">
-                        <Link to='/admin'>
-                            {props.username}
+                        </div>
+                        <div className="button">
+                            <Link to='/men'>
+                                Men
                         </Link>
-                    </div>
-                    <div className="button-register">
-                        <Link to='/' onClick={props.logout}>
-                            Log Out
-                    </Link>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-        console.log('ini belom login')
-        return (
-            <div className="navbar-wrapper">
-                <div className="nav-items">
-                    <div className="logo">
-                        <Link to='/'>
-                            Shuu
-                </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/men'>
-                            Men
-                    </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/women'>
-                            Women
-                    </Link>
-                    </div>
-                    <div className="button">
-                        <Link to='/feeds'>
-                            Feed
-                    </Link>
-                    </div>
-                    <div className="searchbar">
-                        <input type="text" placeholder="search"></input>
-                        <button><i className="fas fa-search"></i></button>
-                    </div>
-                    <div className="button-login">
-                        <Link to='/login'>
-                            Log in
-                    </Link>
-                    </div>
-                    <div className="button-register">
-                        <Link to='/register'>
-                            Register
-                    </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/women'>
+                                Women
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/feeds'>
+                                Feed
+                        </Link>
+                        </div>
+                        <div className="searchbar">
+                            <input type="text" placeholder="search"></input>
+                            <button><i className="fas fa-search"></i></button>
+                        </div>
+                        <div className="button-login">
+                            <Link to='/admin'>
+                                {this.props.username}
+                            </Link>
+                        </div>
+                        <div className="button-register">
+                            <Link to='/' onClick={this.props.logout}>
+                                Log Out
+                        </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            console.log('ini belom login')
+            return (
+                <div className="navbar-wrapper">
+                    <div className="nav-items">
+                        <div className="logo">
+                            <Link to='/'>
+                                Shuu
+                    </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/men'>
+                                Men
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/women'>
+                                Women
+                        </Link>
+                        </div>
+                        <div className="button">
+                            <Link to='/feeds'>
+                                Feed
+                        </Link>
+                        </div>
+                        <div className="searchbar">
+                            <input type="text" placeholder="search"></input>
+                            <button><i className="fas fa-search"></i></button>
+                        </div>
+                        <div className="button-login">
+                            <Link to='/login'>
+                                Log in
+                        </Link>
+                        </div>
+                        <div className="button-register">
+                            <Link to='/register'>
+                                Register
+                        </Link>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+
     }
 }
 
